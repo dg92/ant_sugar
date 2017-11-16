ae05eed7b1ee86041e94dda993db5d12bcea59b1
admindss <admindss@apples-MacBook-Air.local>
2017-11-16 23:11:23 +0530

    gen add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Modified Ant food problem using genetic algorithms, passing good path traits to next generation.
+
+After few generations the ants figure out the path to sugar / food and the next generation use the traits to follow the same for survival.
--- antSugar.js
+++ antSugar.js
@@ -1,6 +1,5 @@
 var population;
 var lifeSpan = 250;
-var lifeOfAntPara;
 var count = 0;
 var sugar;
 var ox = 100;
@@ -8,25 +7,28 @@ var oy = 150;
 var ow = 200;
 var oh = 10;
 var maxForce = 0.2;
+var generationCount = 0;
 
 function setup() {
   createCanvas(400, 300);
   population = new Population();
-  lifeOfAntPara = createP();
+  generationCount++;
   sugar = createVector(width/2, 50);
 }
 
 function draw() {
   background(0);
   population.run();
-  lifeOfAntPara.html(count);
+  textSize(20);
+  fill(255, 255, 255);
+  text("Generation count " + generationCount, 120, 25);
   if (count === lifeSpan) {
     population.evaluate();
     population.selection();
     count = 0;
+    generationCount++;
   }
   count++;
   rect(ox, oy, ow, oh);
-  // rectMode(CENTER);
   rect(sugar.x, sugar.y, 10, 10);
 }
